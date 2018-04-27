d12863e15cee2f185a6c0dfc3edc44d66eded159
andre.arc <andrecosta.cefet@gmail.com>
2018-04-27 15:11:22 -0400

    Splash Screen and Login added

--- app/src/main/java/br/gov/dpf/intelitrack/InitialActivity.java
+++ app/src/main/java/br/gov/dpf/intelitrack/InitialActivity.java
@@ -1,90 +1,38 @@
 package br.gov.dpf.intelitrack;
 
-import android.Manifest;
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
 
-import com.heinrichreimersoftware.materialintro.app.IntroActivity;
-import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
-import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
-public class InitialActivity extends IntroActivity {
+public class InitialActivity extends AppCompatActivity
+{
+    public static final String PREF_KEY_FIRST_START = "com.heinrichreimersoftware.materialintro.demo.PREF_KEY_FIRST_START";
+    public static final int REQUEST_CODE_INTRO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
-        //Set full screen intro
-        setFullscreen(true);
+        boolean firstStart = PreferenceManager.getDefaultSharedPreferences(this).getBoolean(PREF_KEY_FIRST_START, true);
 
-        //Define skip intro click behavior
-        View.OnClickListener skipIntro = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToLastSlide();
-            }
-        };
+        if (firstStart)
+        {
+            Intent intent = new Intent(this, FirstStartActivity.class);
+            startActivityForResult(intent, REQUEST_CODE_INTRO);
+        }
 
-        //Fist slide - Welcome
-        addSlide(new SimpleSlide.Builder()
-                .titleHtml("Bem vindo ao <b><u>InteliTrack</u></b>")
-                .description("Plataforma de rastreamento de ativos desenvolvido no âmbito da Delegacia de Polícia Federal de Naviraí/MS.")
-                .image(R.drawable.map_intro)
-                .background(R.color.page_welcome)
-                .backgroundDark(R.color.page_welcome_dark)
-                .scrollable(true)
-                .buttonCtaLabel("Pular introdução")
-                .buttonCtaClickListener(skipIntro)
-                .build());
+    }
 
-        //Second slide - Goals
-        addSlide(new SimpleSlide.Builder()
-                .titleHtml("Objetivo da plataforma")
-                .description("A principal finalidade deste sistema é permitir, de forma simples e padronizada, o gerenciamento de diversos modelos de dispositivos rastreadores.")
-                .image(R.drawable.goal_intro)
-                .background(R.color.page_goals)
-                .backgroundDark(R.color.page_goals_dark)
-                .scrollable(true)
-                .buttonCtaLabel("Pular introdução")
-                .buttonCtaClickListener(skipIntro)
-                .build());
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        //Third slide - Features
-        addSlide(new SimpleSlide.Builder()
-                .titleHtml("Principais funcionalidades")
-                .description("Notificações em tempo real, histórico de localizações, configurações personalizadas para cada modelo de rastreador, entre outras a serem adicionadas.")
-                .image(R.drawable.features_intro)
-                .background(R.color.page_features)
-                .backgroundDark(R.color.page_features_dark)
-                .scrollable(true)
-                .buttonCtaLabel("Pular introdução")
-                .buttonCtaClickListener(skipIntro)
-                .build());
-        //Fourth slide - Development
-        addSlide(new SimpleSlide.Builder()
-                .titleHtml("Desenvolvimento contínuo")
-                .description("Por se tratar de uma plataforma em desenvolvimento, sugestões de melhoria serão bem vindas aos administradores do sistema.")
-                .image(R.drawable.development_intro)
-                .background(R.color.page_development)
-                .backgroundDark(R.color.page_development_dark)
-                .scrollable(true)
-                .buttonCtaLabel("Pular introdução")
-                .buttonCtaClickListener(skipIntro)
-                .build());
-
-        LoginFragment loginFragment = LoginFragment.newInstance();
-
-        addSlide(new FragmentSlide.Builder()
-                .background(R.color.page_login)
-                .backgroundDark(R.color.page_login_dark)
-                .fragment(loginFragment)
-                .buttonCtaLabel("Acessar sistema")
-                .buttonCtaClickListener(loginFragment.performLogin)
-                .build());
-
-
+        Intent intent = new Intent(InitialActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
--- app/src/main/java/br/gov/dpf/intelitrack/LoginActivity.java
+++ app/src/main/java/br/gov/dpf/intelitrack/LoginActivity.java
@@ -63,80 +63,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private View mLoginFormView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
-
-        mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
-                    return true;
-                }
-                return false;
-            }
-        });
-
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptLogin();
-            }
-        });
-
-        mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
     }
-
-    private void populateAutoComplete() {
-        if (!mayRequestContacts()) {
-            return;
-        }
-
-        getLoaderManager().initLoader(0, null, this);
-    }
-
-    private boolean mayRequestContacts() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
-        }
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        }
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener() {
-                        @Override
-                        @TargetApi(Build.VERSION_CODES.M)
-                        public void onClick(View v) {
-                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-                        }
-                    });
-        } else {
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-        }
-        return false;
-    }
-
-    /**
-     * Callback received when a permissions request has been completed.
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_READ_CONTACTS) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                populateAutoComplete();
-            }
-        }
-    }
-
 
     /**
      * Attempts to sign in or register the account specified by the login form.
